e33943675137aaf766e6609019336e181af1e6b6
Parthiv-P-maker <parthivp303@gmail.com>
2025-10-16 21:30:45 +0530

    text1

--- appjava/src/main/java/com/attendddd/TestDB.java
+++ appjava/src/main/java/com/attendddd/TestDB.java
@@ -4,12 +4,12 @@ import com.attendddd.util.DBConnection;
 import java.sql.Connection;
 
 public class TestDB {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
             System.out.println("Connected to MySQL successfully!");
         } catch (Exception e) {
-            e.printStackTrace();//vvv
-            
+            e.printStackTrace();// vvv
+
         }
-    }   
+    }
 }
